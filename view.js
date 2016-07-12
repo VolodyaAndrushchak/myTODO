2f29387f57ea8c124110331a1045b5998d3e16c7
VolodyaAndrushchak <volodya1andrushchak@gmail.com>
2016-07-13 00:16:03 +0300

    addStyle

--- view.js
+++ view.js
@@ -3,7 +3,7 @@ var viewMainList = function(content, callback){
 	var htmlContent = '';
 	for(var i = 0; i < content.length; i++)
 	{
-		htmlContent = htmlContent + "<li class='liTodo'><div class='TaskContent'  onclick='clickList(this);'> <div class='textContent'>"+ content[i].taskName + "</div><div class='time1'>" + content[i].time1 + "</div> <div class='time2'>" + content[i].time2 + "</div> <div class='priority'>" + content[i].priority + "</div><input value = 'delete' type='button' class='delete' onclick= 'deleteTask(this);'> <input value = 'done' type='button' class='done'> </div> <div class = 'popUpGRUD'> <input class='tastInput'>time <input class = 'time1Pop'>to<input class = 'time2Pop'>priority<select class = 'newPriority'><option>A</option><option>B</option><option>C</option></select><input class = 'editAdd' onclick= 'editADD(this)' value ='edit' type='button'> </div> </li> "
+		htmlContent = htmlContent + "<li class='liTodo'><div class='TaskContent'  onclick='clickList(this);'> <div class='textContent'>"+ content[i].taskName + "</div><div class='time1'>" + content[i].time1 + "</div> <div class='time2'>" + content[i].time2 + "</div> <div class='priority'>" + content[i].priority + "</div><div class='delete' onclick= 'deleteTask(this);'> delete</div>  <div class='done'>done</div> </div> <div class = 'popUpGRUD'><div class = 'popInput'><input class='tastInput'>time <input class = 'time1Pop'>to<input class = 'time2Pop'>priority<select class = 'newPriority'><option>A</option><option>B</option><option>C</option></select></div><div class = 'editAdd' onclick= 'editADD(this)'>edit</div> </li> "
 	}
 	var statusDay = '';
 	if(content.length <= 3)
